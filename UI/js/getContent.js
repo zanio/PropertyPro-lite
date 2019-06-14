e1c6afc6e3d4a2829c1dc34f29dfa6544b418089
Zanio <akp.ani@yahoo.com>
2019-06-14 15:49:29 +0100

    chore(javacrpt-fetch): add javascript function to fecth header and footer content [fixed #166656227]

---
+++ UI/js/getContent.js
@@ -0,0 +1,34 @@
+const footerURL = './navigation/footer.html';
+const headerURL = './navigation/header.html';
+const formURL = './form/form-1.html';
+
+const getPageContent = (url)=>{
+return new Promise((resolve, reject)=>{
+        if(window.fetch){
+        resolve(fetch(url))
+        reject(err)
+        } else{
+            reject(err)
+        }
+    });
+};
+
+(function(){
+    getPageContent(headerURL)
+    .then(res=>res.text())
+   .then(res=> document.querySelector('.insertHeader').insertAdjacentHTML('beforeend',res))
+   .catch(err=>{console.log(err)})
+   
+   getPageContent(footerURL)
+    .then(res=>res.text())
+   .then(res=> document.querySelector('.insertFooter').insertAdjacentHTML('beforeend',res))
+   .catch(err=>{console.log(err)})
+
+//    getPageContent(formURL)
+//    .then(res=>res.text())
+//   .then(res=> document.querySelector('.insertForm').insertAdjacentHTML('beforeend',res))
+//   .catch(err=>{console.log(err)})
+   
+   
+   })()
+
