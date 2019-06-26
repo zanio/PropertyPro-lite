ca2aee1ae3b31abb6d584aba1bf5e9d57eb9a928
Zanio <Akp.ani@yahoo.com>
2019-06-26 17:53:14 +0100

    chore(test):fix testing enviroment

--- src/test/account.spec.js
+++ src/test/account.spec.js
@@ -26,7 +26,7 @@ describe('/GET user account info', () => {
 	it('it should GET authenticated user ', (done) => {
 		chai.request(app)
 			.get('/api/v1/auth/my-account/*')
-			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJHAzVk5XTkFNTFcuSTRZVWZ3NDlkVWVxRG5WZDg1S1BXUHpGalk4YmFVc0tRRHJWaEpuOURxIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6ImRkZiIsImxhc3RfbmFtZSI6InZkc3ZkIiwiZW1haWwiOiJhZGV3YWxlLm9sYW95b0B5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRwM1ZOV05BTUxXLkk0WVVmdzQ5ZFVlcURuVmQ4NUtQV1B6RmpZOGJhVXNLUURyVmhKbjlEcSIsImFkZHJlc3MiOiJzZmZmIiwicGhvbmVfbnVtYmVyIjoiMDkwMTIzNDUzMjEiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOnRydWV9LCJpYXQiOjE1NjE1NjM2MTV9.6ln1TUdyMqRJIfxq-WDjq-D8h-wGinH-ffLnWb86YYQ')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJDJoUk1rRVRvZjJVSVZvWWhDRWhxak9BMWlHNUhMZ3E4NE9mbFpyN0pMcHhTRVhZMEdoc2hxIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6ImRkZiIsImxhc3RfbmFtZSI6InZkc3ZkIiwiZW1haWwiOiJhZGV3YWxlLm9sYW95b0B5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQyaFJNa0VUb2YyVUlWb1loQ0VocWpPQTFpRzVITGdxODRPZmxacjdKTHB4U0VYWTBHaHNocSIsImFkZHJlc3MiOiJzZmZmIiwicGhvbmVfbnVtYmVyIjoiMDkwMTIzNDUzMjEiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOnRydWV9LCJpYXQiOjE1NjE1NjcwMjh9.dIx582qdzBMoTA8mF9NZPb67HORtEcZEH1riWe0Jf6o')
 			.end((err, res) => {
 				expect(res.status).to.equal(200);
 				done();
