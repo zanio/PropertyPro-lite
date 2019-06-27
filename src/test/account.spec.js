4cd9d876d74c6f4814fc7297c98d689aedad4e3d
Zanio <Akp.ani@yahoo.com>
2019-06-27 09:40:39 +0100

    chore(travis):fix travis ci fail [Test #166847045]

--- src/test/account.spec.js
+++ src/test/account.spec.js
@@ -26,7 +26,7 @@ describe('/GET user account info', () => {
 	it('it should GET authenticated user ', (done) => {
 		chai.request(app)
 			.get('/api/v1/auth/my-account/*')
-			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJDJoUk1rRVRvZjJVSVZvWWhDRWhxak9BMWlHNUhMZ3E4NE9mbFpyN0pMcHhTRVhZMEdoc2hxIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6ImRkZiIsImxhc3RfbmFtZSI6InZkc3ZkIiwiZW1haWwiOiJhZGV3YWxlLm9sYW95b0B5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQyaFJNa0VUb2YyVUlWb1loQ0VocWpPQTFpRzVITGdxODRPZmxacjdKTHB4U0VYWTBHaHNocSIsImFkZHJlc3MiOiJzZmZmIiwicGhvbmVfbnVtYmVyIjoiMDkwMTIzNDUzMjEiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOnRydWV9LCJpYXQiOjE1NjE1NjcwMjh9.dIx582qdzBMoTA8mF9NZPb67HORtEcZEH1riWe0Jf6o')
+			.set('Authorization','bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJDJoUk1rRVRvZjJVSVZvWWhDRWhxak9BMWlHNUhMZ3E4NE9mbFpyN0pMcHhTRVhZMEdoc2hxIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6ImRkZiIsImxhc3RfbmFtZSI6InZkc3ZkIiwiZW1haWwiOiJhZGV3YWxlLm9sYW95b0B5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCQyaFJNa0VUb2YyVUlWb1loQ0VocWpPQTFpRzVITGdxODRPZmxacjdKTHB4U0VYWTBHaHNocSIsImFkZHJlc3MiOiJzZmZmIiwicGhvbmVfbnVtYmVyIjoiMDkwMTIzNDUzMjEiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOnRydWV9LCJpYXQiOjE1NjE1NjcwMjh9.dIx582qdzBMoTA8mF9NZPb67HORtEcZEH1riWe0Jf6o')
 			.end((err, res) => {
 				expect(res.status).to.equal(200);
 				done();
