e324a52ead024b7bd14c3a0985942e18690a13c5
Zanio <akp.ani@yahoo.com>
2019-06-28 12:44:09 +0100

    feature(property-advert-routes):add single property type route [ Finish #166964798]

--- src/routes/property-advert.routes.js
+++ src/routes/property-advert.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const propertyrouter = express.Router();
 import {multerUploads} from '../middlewares/multer';
+import url from 'url';
 
 import {cloudinaryHandler} from '../middlewares/cloudinary';
-const propertyadvert = require('../models/property-advert.model');
-import {authorization,getId,getPreviousId,idCheck,toDeleteId} from '../middlewares/auth/auth';
+import {getPropertyAdvert,getPropertyAdverts,updatePropertyAdvert,deletePropertyAdvert,insertPropertyAdvert,getTypeProperty} from '../models/property-advert.model';
+import {authorization,getId,getPreviousId,idCheck,toDeleteId,getSingleIdProperty} from '../middlewares/auth/auth';
 import {jwtVerify} from '../middlewares/auth/jsonweb';
 import {checkPropertyEmpty,checkPropertyField} from '../middlewares/field/inputfield';
 
@@ -14,28 +15,43 @@ import {checkPropertyEmpty,checkPropertyField} from '../middlewares/field/inputf
 propertyrouter.post('/property-advert', authorization, 
 	jwtVerify,multerUploads,checkPropertyEmpty,checkPropertyField,cloudinaryHandler, getId,async(req, res)=>{
 		const {data} = req;
-		propertyadvert.insertPropertyAdvert(data)
+		insertPropertyAdvert(data)
 			.then(response=>{
 				res.status(200).json({status:200,data:response});
 			});
   
 	});
 
+
 propertyrouter.patch('/property-advert/:id', idCheck, authorization, 
 	jwtVerify,multerUploads,checkPropertyEmpty,checkPropertyField,cloudinaryHandler, getPreviousId,async(req, res)=>{
 		const {data} = req;
 		const id = req.params.id;	
-		propertyadvert.updatePropertyAdvert(id,data)
+		updatePropertyAdvert(id,data)
 			.then(response=>{
 				res.status(200).json({status:200,data:response});
 			}).catch(err=>res.status(404).json({status: 404, err: err.message}));
 			
 	});
 
+
+propertyrouter.patch('/property-advert/:id/sold', idCheck, authorization, jwtVerify, getSingleIdProperty,async(req, res)=>{	
+	const {data} = req;
+	const id = req.params.id;	
+	updatePropertyAdvert(id,data)
+		.then(response=>{
+			res.status(200).json({status:200,data:response});
+		}).catch(err=>res.status(404).json({status: 404, err: err.message}));
+			
+});
+
+
+	
+
 propertyrouter.delete('/property-advert/:id', idCheck, authorization, 
 	jwtVerify, toDeleteId,async(req, res)=>{
 		const id = req.params.id;	
-		propertyadvert.deletePropertyAdvert(id)
+		deletePropertyAdvert(id)
 			// eslint-disable-next-line no-unused-vars
 			.then(response=>{
 				res.status(201).json({status:200,data:{message:`advert with the ${id} id has been successfully deleted`}});
@@ -44,18 +60,30 @@ propertyrouter.delete('/property-advert/:id', idCheck, authorization,
 	});
 
 propertyrouter.get('/property-advert/',async(req, res)=>{
-	propertyadvert.getPropertyAdverts()
+	
+	getPropertyAdverts()
 		.then(response=>{
 			res.status(201).json({status:200,data:response});
 		}).catch(err=>res.status(404).json({status: 404, err: err.message}));
 			
 });
 
-propertyrouter.get('/property-advert/?q',async(req, res)=>{
-	propertyadvert.getPropertyAdverts()
+propertyrouter.get('/property-advert/search',async(req, res)=>{
+	const url_parts = url.parse(req.url,true).query;
+	getTypeProperty(url_parts.type)
+		.then(response=>{
+			res.status(201).json({status:200,data:response});
+		}).catch(err=>res.status(404).json({status: 404, err: err.err}));
+		
+			
+});
+
+propertyrouter.get('/property-advert/:id',async(req, res)=>{
+	getPropertyAdvert(req.params.id)
 		.then(response=>{
 			res.status(201).json({status:200,data:response});
 		}).catch(err=>res.status(404).json({status: 404, err: err.message}));
+		
 			
 });
 
