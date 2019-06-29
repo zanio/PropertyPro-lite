ce98ef9e8ead047b5448f839bd324faf4d03152d
Zanio <akp.ani@yahoo.com>
2019-06-29 03:51:08 +0100

    chore(merge):fix local merge issue

--- UI/js/getContent.js
+++ UI/js/getContent.js
@@ -1,83 +1,84 @@
+/* eslint-disable no-console */
 const page = {
-    footer:{
-        url: "./navigation/footer.html",
-        element: ".insertFooter"
-    },
+	footer:{
+		url: './navigation/footer.html',
+		element: '.insertFooter'
+	},
 
-    header:{
-        url: "./navigation/header.html",
-        element: ".insertHeader"
-    },
+	header:{
+		url: './navigation/header.html',
+		element: '.insertHeader'
+	},
 
-    authHeader:{
-        url: "./navigation/authheader.html",
-        element: ".insertauthHeader"
-    },
+	authHeader:{
+		url: './navigation/authheader.html',
+		element: '.insertauthHeader'
+	},
 
-    form:{
-        url: "./form/form-1.html",
-        element: ".insertForm"
-    },
+	form:{
+		url: './form/form-1.html',
+		element: '.insertForm'
+	},
 
-    update:{
-        url: "./form/update/form-1.html",
-        element: ".insertUpdate"
-    }
+	update:{
+		url: './form/update/form-1.html',
+		element: '.insertUpdate'
+	}
     
     
-}
+};
 
 
 let getPage = function(){
-    return location.pathname.split('/')[2]
-}
+	return location.pathname.split('/')[2];
+};
 
 const getPageContent = (url)=>{
-    return new Promise((resolve, reject)=>{
-            if(typeof window.fetch !== 'undefined'){
-                fetch(url)
-                .then(res => {
-                    resolve(res)                
-                }).catch(err =>{
-                    reject(err)
-                })
+	return new Promise((resolve, reject)=>{
+		if(typeof window.fetch !== 'undefined'){
+			fetch(url)
+				.then(res => {
+					resolve(res);                
+				}).catch(err =>{
+					reject(err);
+				});
 
-            } else{
-                reject(err)
-            }
-        });
+		} else{
+			reject(err);
+		}
+	});
 };
 
 
 const loadPage = function(url , element,cb){
 
-    getPageContent(url)
-    .then(res=>res.text())
-    .then(res=>{
-        document.querySelector(element).insertAdjacentHTML('beforeend' , res)
-        cb ?cb():null;
-    } )
-    .catch(err=>{console.log(err)})
-}
+	getPageContent(url)
+		.then(res=>res.text())
+		.then(res=>{
+			document.querySelector(element).insertAdjacentHTML('beforeend' , res);
+			cb ?cb():null;
+		} )
+		.catch(err=>{console.log(err);});
+};
 
 
 //load footer on all pages
 if(getPage() !== null)
-    loadPage(page.footer.url , page.footer.element)
+	loadPage(page.footer.url , page.footer.element);
 
 
 
 
-if( getPage() === "home.html" ||
-    getPage() === "signup.html" ||
-    getPage() === "all-property.html" ||
-    getPage() === "single-property.html" ||
-    getPage() === "flat.html" ||
-    getPage() === "miniflat.html" ||
-    getPage() === "land.html" ||
-    getPage() === "real-estate.html" ||
-    getPage() === "login.html" )
-    loadPage(page.header.url , page.header.element)   
+if( getPage() === 'home.html' ||
+    getPage() === 'signup.html' ||
+    getPage() === 'all-property.html' ||
+    getPage() === 'single-property.html' ||
+    getPage() === 'flat.html' ||
+    getPage() === 'miniflat.html' ||
+    getPage() === 'land.html' ||
+    getPage() === 'real-estate.html' ||
+    getPage() === 'login.html' )
+	loadPage(page.header.url , page.header.element);   
 
 
     
@@ -85,20 +86,20 @@ if( getPage() ==='dashboard.html' ||
     getPage() ==='update.html' ||
     getPage() ==='no-advert.html' ||
     getPage() ==='post.html' 
-    )
-    loadPage(page.authHeader.url , page.authHeader.element) 
+)
+	loadPage(page.authHeader.url , page.authHeader.element); 
 
 if( getPage() ==='dashboard.html' &&
     getPage() ==='flat.html'
-    )
-    loadPage(page.authHeader.url , page.authHeader.element) 
+)
+	loadPage(page.authHeader.url , page.authHeader.element); 
 
 if( getPage() ==='update.html')
-    loadPage(page.update.url , page.update.element) 
+	loadPage(page.update.url , page.update.element); 
     
 
 
 if( getPage() ==='post.html')
-    loadPage(page.form.url , page.form.element)
+	loadPage(page.form.url , page.form.element);
 
 
--- UI/js/mains.js
+++ UI/js/mains.js
@@ -1,155 +1,158 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
 window.addEventListener('load', ()=>{
     
-const bars = document.querySelector('.bars');
+	const bars = document.querySelector('.bars');
 
-const btnarr = (btn)=>{
-    const updatedom = document.querySelectorAll(btn);
-    let actionclasses =[];
-    for(let i =0; i < updatedom.length; i++){
-        actionclasses.push(updatedom[i])
-    }
-    return actionclasses
-}
+	const btnarr = (btn)=>{
+		const updatedom = document.querySelectorAll(btn);
+		let actionclasses =[];
+		for(let i =0; i < updatedom.length; i++){
+			actionclasses.push(updatedom[i]);
+		}
+		return actionclasses;
+	};
 
-const updateOrder = (id)=>{
-    document.querySelector(id).addEventListener('click', ()=>{
-        location.href="dashboard.html"
-     })
-    return 
-}
-
-
-
-const userDp = document.querySelector('.user-setting');
-
-const removeDomElement = (cls)=>{
-    const elem = document.querySelector(cls);
-    return  elem.parentNode.removeChild(elem);
-}
+	const updateOrder = (id)=>{
+		document.querySelector(id).addEventListener('click', ()=>{
+			location.href='dashboard.html';
+		});
+		return; 
+	};
 
 
 
-function barss(){
+	const userDp = document.querySelector('.user-setting');
+
+	const removeDomElement = (cls)=>{
+		const elem = document.querySelector(cls);
+		return  elem.parentNode.removeChild(elem);
+	};
+
+
+
+	function barss(){
     
-    document.querySelector('.mobile').style.display = "block";
-    document.querySelector('body')
-    .insertAdjacentHTML('beforeend','<div class="backdrop"></div>')
+		document.querySelector('.mobile').style.display = 'block';
+		document.querySelector('body')
+			.insertAdjacentHTML('beforeend','<div class="backdrop"></div>');
 
-    document.querySelector('.backdrop').addEventListener('click',function(){
-        this.style.display = "none";
-        document.querySelector('.mobile').style.display = "none";
-        this.parentNode.removeChild(this);
-    })
-    return;
-}
+		document.querySelector('.backdrop').addEventListener('click',function(){
+			this.style.display = 'none';
+			document.querySelector('.mobile').style.display = 'none';
+			this.parentNode.removeChild(this);
+		});
+		return;
+	}
 
 
 
-document.querySelector('body').addEventListener('click',(e)=>{
-    const id = e.target.id
-    const classList = e.target.classList.value;
-    if(classList === "bars"){
-        barss()
-    }
+	document.querySelector('body').addEventListener('click',(e)=>{
+		const id = e.target.id;
+		const classList = e.target.classList.value;
+		if(classList === 'bars'){
+			barss();
+		}
    
-    if(id === 'next-1' ){
-        document.querySelector('.inactive-circle').classList.remove('inactive-circle')
-        removeDomElement('.form-bg');
-        const formURL = './form/form-2.html';
-        loadPage(formURL , '.insertForm',function(){
-            location.hash = 'example';
-        })
-        return
-    }
+		if(id === 'next-1' ){
+			document.querySelector('.inactive-circle').classList.remove('inactive-circle');
+			removeDomElement('.form-bg');
+			const formURL = './form/form-2.html';
+			loadPage(formURL , '.insertForm',function(){
+				location.hash = 'example';
+			});
+			return;
+		}
 
-    if(id === 'next-2' ){
-        if(id === 'cir-1'){
-            const formURL = './form/form-1.html';
-            removeDomElement('.form-bg');
-            loadPage(formURL , '.insertForm')
-            return
-           }
+		if(id === 'next-2' ){
+			if(id === 'cir-1'){
+				const formURL = './form/form-1.html';
+				removeDomElement('.form-bg');
+				loadPage(formURL , '.insertForm');
+				return;
+			}
 
-        document.querySelector('.inactive-circle').classList.remove('inactive-circle')
-        removeDomElement('.form-bg');
-        const formURL = './form/feedback.html';
-        loadPage(formURL , '.insertForm',function(){
-            location.hash = 'example';
-        })
-        return
-    }
+			document.querySelector('.inactive-circle').classList.remove('inactive-circle');
+			removeDomElement('.form-bg');
+			const formURL = './form/feedback.html';
+			loadPage(formURL , '.insertForm',function(){
+				location.hash = 'example';
+			});
+			return;
+		}
 
-    if(id === 'update-1' ){
+		if(id === 'update-1' ){
        
-        document.querySelector('.inactive-circle').classList.remove('inactive-circle')
-        removeDomElement('.form-bg');
-        const formURL = './form/update/form-2.html';
-        loadPage(formURL , '.insertUpdate')
+			document.querySelector('.inactive-circle').classList.remove('inactive-circle');
+			removeDomElement('.form-bg');
+			const formURL = './form/update/form-2.html';
+			loadPage(formURL , '.insertUpdate');
 
         
-    }
-    if(id === 'update-2' ){
-        document.querySelector('.inactive-circle').classList.remove('inactive-circle')
-        removeDomElement('.form-bg');
-        const formURL = './form/update/feedback.html';
-        loadPage(formURL , '.insertUpdate')
-        return
+		}
+		if(id === 'update-2' ){
+			document.querySelector('.inactive-circle').classList.remove('inactive-circle');
+			removeDomElement('.form-bg');
+			const formURL = './form/update/feedback.html';
+			loadPage(formURL , '.insertUpdate');
+			return;
   
-    }
+		}
 
 
-}) 
+	}); 
 
 
-btnarr('.delete') !== null ?
-btnarr('.delete').map(el=>{
-    el.addEventListener('click',()=>{
-        const modalURL = './modal/modal.html';
-        getPageContent(modalURL)
-        .then(res=>res.text())
-        .then(res=> document.querySelector('body').insertAdjacentHTML('beforeend',res))
-        .then(()=>{
-            if(document.querySelector('#modal')){
-               updateOrder('#modal')
-            }
-        })
+	btnarr('.delete') !== null ?
+		btnarr('.delete').map(el=>{
+			el.addEventListener('click',()=>{
+				const modalURL = './modal/modal.html';
+				getPageContent(modalURL)
+					.then(res=>res.text())
+					.then(res=> document.querySelector('body').insertAdjacentHTML('beforeend',res))
+					.then(()=>{
+						if(document.querySelector('#modal')){
+							updateOrder('#modal');
+						}
+					})
             
-        .catch(err=>{console.log(err)})
+					.catch(err=>{console.log(err);});
         
-    }) 
-})
-: null;
+			}); 
+		})
+		: null;
 
-btnarr('.mark-advert') !== null ?
-btnarr('.mark-advert').map(el=>{
-    el.addEventListener('click',()=>{
-        const modalURL = './modal/modal-update.html';
-        getPageContent(modalURL)
-        .then(res=>res.text())
-        .then(res=> document.querySelector('body').insertAdjacentHTML('beforeend',res))
-        .then(()=>{
-            if(document.querySelector('#modal-update')){
-               updateOrder('#modal-update')
-            }
-        })
+	btnarr('.mark-advert') !== null ?
+		btnarr('.mark-advert').map(el=>{
+			el.addEventListener('click',()=>{
+				const modalURL = './modal/modal-update.html';
+				getPageContent(modalURL)
+					.then(res=>res.text())
+					.then(res=> document.querySelector('body').insertAdjacentHTML('beforeend',res))
+					.then(()=>{
+						if(document.querySelector('#modal-update')){
+							updateOrder('#modal-update');
+						}
+					})
             
-        .catch(err=>{console.log(err)})
+					.catch(err=>{console.log(err);});
         
-    }) 
-})
-: null;
+			}); 
+		})
+		: null;
 
-document.querySelector('.user-dp') !== null ?
-document.querySelector('body').addEventListener('click',(e)=>{
-    if(e.target.classList.value === 'user-dp'){
-        document.querySelector('.user-setting').style.cssText = 'display:block !important';
-    }
-    else{
-    document.querySelector('.user-setting').style.cssText = 'display:none !important';
-    }
+	document.querySelector('.user-dp') !== null ?
+		document.querySelector('body').addEventListener('click',(e)=>{
+			if(e.target.classList.value === 'user-dp'){
+				document.querySelector('.user-setting').style.cssText = 'display:block !important';
+			}
+			else{
+				document.querySelector('.user-setting').style.cssText = 'display:none !important';
+			}
     
-})
-: null;
+		})
+		: null;
 
 });
 
