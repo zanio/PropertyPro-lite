1eaf7a5645ab4dd7d67a4ab0f26feb14c830a09a
Zanio <akp.ani@yahoo.com>
2019-06-29 05:36:57 +0100

    chore(test-enpoint):add test for property post endpoint, user endpoint and some utility function [Start #166994978]

--- src/test/property-advert.spec.js
+++ src/test/property-advert.spec.js
@@ -3,7 +3,6 @@ import chai from 'chai';
 import {expect} from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app';
-import fs from 'fs';
 
 
 
@@ -15,9 +14,9 @@ chai.use(chaiHttp);
   * Test the Car /POST route
   */
 describe(' POST AD ROUTES', () => {
-	it('it should create new property advert ', (done) => {
+	it('it should create new property advert ', function (done) {
     
-    
+		this.timeout(10000);
     
 		const  body = {
 			status:'sold',
@@ -33,35 +32,143 @@ describe(' POST AD ROUTES', () => {
 			note:'just be a good buyer'
 		};
     
-	
-
-	
   
 		chai.request(app)
 			.post('/api/v1/property-advert')
-			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJENGcU1TTUdybEVYazBQZ2ZsREhQSHVYeVI1RkNBaE1LOUZLaEFHM1BKVDF1TU9MNjV5dXdHIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6ImRkZiIsImxhc3RfbmFtZSI6InZkc3ZkIiwiZW1haWwiOiJhZGV3YWxlLm9sYW95b0B5YWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRDRnFNU01HcmxFWGswUGdmbERIUEh1WHlSNUZDQWhNSzlGS2hBRzNQSlQxdU1PTDY1eXV3RyIsImFkZHJlc3MiOiJzZmZmIiwicGhvbmVfbnVtYmVyIjoiMDkwMTIzNDUzMjEiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOnRydWV9LCJpYXQiOjE1NjE2MjU4ODl9.Tn5VJbvjwxuUuXTskV7HVWvtAb0nOfBkPO9RlYL2PFM')
-			.type('form')
-			.attach('file','src/test/Screenshot (24).png')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJFZBa01LVXVtaEdLWUxkbEZkc0lwVS54aS5mMGdSbnNwMTY1WXJCVUd4SUZsUmQvR1VpTFhLIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6Im11c2EiLCJsYXN0X25hbWUiOiJmZWtsZ2dpeCIsImVtYWlsIjoiZGFiY2JnZHlAYWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWQWtNS1V1bWhHS1lMZGxGZHNJcFUueGkuZjBnUm5zcDE2NVlyQlVHeElGbFJkL0dVaUxYSyIsImFkZHJlc3MiOiJibG9jayAxOTkgZmxhdCA0IiwicGhvbmVfbnVtYmVyIjoiMDkwODc4NTY3ODQiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTYxNzc3NTQ1fQ.2rYG5nfwuB5FWXeq1cfu_DJKIfiDWx8B4LjjrlrCtkg')
+			.attach('image','src/test/Screenshot (24).png')
 			.field(body)
 			.end((err, res) => {
+				expect(err).to.equal(null);
 				expect(res.status).to.equal(200);
 				expect(res.body).to.be.a('object');
 				done();
 			});
 	});
 
-	it('it shold return 401 unauthorized ', (done) => {
-		
-		const body = {
+	it('it should return 403 with state can not contain number ', function (done) {
+    
+    
+		const  body = {
+			status:'sold',
+			price:'12443.44',
+			state:'lag1os',
+			city:'vi',
+			address:'block 188',
+			type:'flat',
+			contact_person_number:'098123456732',
+			property_name:'adewale',
+			contact_person_address:'block 199 flat 4',
+			proof:'yes i do',
+			note:'just be a good buyer'
+		};
+    
+  
+		chai.request(app)
+			.post('/api/v1/property-advert')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJFZBa01LVXVtaEdLWUxkbEZkc0lwVS54aS5mMGdSbnNwMTY1WXJCVUd4SUZsUmQvR1VpTFhLIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6Im11c2EiLCJsYXN0X25hbWUiOiJmZWtsZ2dpeCIsImVtYWlsIjoiZGFiY2JnZHlAYWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWQWtNS1V1bWhHS1lMZGxGZHNJcFUueGkuZjBnUm5zcDE2NVlyQlVHeElGbFJkL0dVaUxYSyIsImFkZHJlc3MiOiJibG9jayAxOTkgZmxhdCA0IiwicGhvbmVfbnVtYmVyIjoiMDkwODc4NTY3ODQiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTYxNzc3NTQ1fQ.2rYG5nfwuB5FWXeq1cfu_DJKIfiDWx8B4LjjrlrCtkg')
+			.attach('image','src/test/Screenshot (24).png')
+			.field(body)
+			.end((err, res) => {
+				expect(res.status).to.equal(403);
+				expect(res.body).to.be.a('object');
+				done();
+			});
+	});
+
+
+	it('it should return 403 with all field required ', function (done) {
+    
+		const  body = {
+			status:'sold',
+			price:'12443.44',
+			type:'flat',
+			contact_person_number:'098123456732',
+			property_name:'adewale',
+			contact_person_address:'block 199 flat 4',
+			proof:'yes i do',
+			note:'just be a good buyer'
+		};
+    
+  
+		chai.request(app)
+			.post('/api/v1/property-advert')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJFZBa01LVXVtaEdLWUxkbEZkc0lwVS54aS5mMGdSbnNwMTY1WXJCVUd4SUZsUmQvR1VpTFhLIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6Im11c2EiLCJsYXN0X25hbWUiOiJmZWtsZ2dpeCIsImVtYWlsIjoiZGFiY2JnZHlAYWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWQWtNS1V1bWhHS1lMZGxGZHNJcFUueGkuZjBnUm5zcDE2NVlyQlVHeElGbFJkL0dVaUxYSyIsImFkZHJlc3MiOiJibG9jayAxOTkgZmxhdCA0IiwicGhvbmVfbnVtYmVyIjoiMDkwODc4NTY3ODQiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTYxNzc3NTQ1fQ.2rYG5nfwuB5FWXeq1cfu_DJKIfiDWx8B4LjjrlrCtkg')
+			.attach('image','src/test/Screenshot (24).png')
+			.field(body)
+			.end((err, res) => {
+				expect(res.status).to.equal(403);
+				expect(res.body).to.be.a('object');
+				done();
+			});
+	});
+
+	it('it should return 403 with error of image field is required ', function (done) {
+    
+	
+		const  body = {
 			status:'sold',
 			price:'12443.44',
 			state:'lagos',
 			city:'vi',
 			address:'block 188',
 			type:'flat',
-      
+			contact_person_number:'098123456732',
+			property_name:'adewale',
+			contact_person_address:'block 199 flat 4',
+			proof:'yes i do',
+			note:'just be a good buyer'
 		};
     
+  
+		chai.request(app)
+			.post('/api/v1/property-advert')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJFZBa01LVXVtaEdLWUxkbEZkc0lwVS54aS5mMGdSbnNwMTY1WXJCVUd4SUZsUmQvR1VpTFhLIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6Im11c2EiLCJsYXN0X25hbWUiOiJmZWtsZ2dpeCIsImVtYWlsIjoiZGFiY2JnZHlAYWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWQWtNS1V1bWhHS1lMZGxGZHNJcFUueGkuZjBnUm5zcDE2NVlyQlVHeElGbFJkL0dVaUxYSyIsImFkZHJlc3MiOiJibG9jayAxOTkgZmxhdCA0IiwicGhvbmVfbnVtYmVyIjoiMDkwODc4NTY3ODQiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTYxNzc3NTQ1fQ.2rYG5nfwuB5FWXeq1cfu_DJKIfiDWx8B4LjjrlrCtkg')
+			.field(body)
+			.end((err, res) => {
+				expect(res.status).to.equal(403);
+				expect(res.body).to.be.a('object');
+				done();
+			});
+	});
+
+	it('it should return 401 with error of token secret mismatch ', function (done) {
+	
+		const  body = {
+			status:'sold',
+			price:'12443.44',
+			type:'flat',
+			contact_person_number:'098123456732',
+			property_name:'adewale',
+			contact_person_address:'block 199 flat 4',
+			proof:'yes i do',
+			note:'just be a good buyer'
+		};
+  
+		chai.request(app)
+			.post('/api/v1/property-advert')
+			.set('Authorization','Bearer eyJhbGciOiJIUzI1biIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiJDJiJDEwJFZBa01LVXVtaEdLWUxkbEZkc0lwVS54aS5mMGdSbnNwMTY1WXJCVUd4SUZsUmQvR1VpTFhLIiwiaWQiOjIzNDAxMzAwNTAwMSwibmV3VXNlck5vdG9rZW4iOnsiZmlyc3RfbmFtZSI6Im11c2EiLCJsYXN0X25hbWUiOiJmZWtsZ2dpeCIsImVtYWlsIjoiZGFiY2JnZHlAYWhvby5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRWQWtNS1V1bWhHS1lMZGxGZHNJcFUueGkuZjBnUm5zcDE2NVlyQlVHeElGbFJkL0dVaUxYSyIsImFkZHJlc3MiOiJibG9jayAxOTkgZmxhdCA0IiwicGhvbmVfbnVtYmVyIjoiMDkwODc4NTY3ODQiLCJnZW5kZXIiOiJtYWxlIiwiaXNfQWRtaW4iOmZhbHNlfSwiaWF0IjoxNTYxNzc3NTQ1fQ.2rYG5nfwuB5FWXeq1cfu_DJKIfiDWx8B4LjjrlrCtkg')
+			.field(body)
+			.end((err, res) => {
+				expect(res.status).to.equal(401);
+				expect(res.body).to.be.a('object');
+				done();
+			});
+	});
+
+
+	it('it shold return 401 unauthorized ', (done) => {
+		const  body = {
+			status:'sold',
+			price:'12443.44',
+			type:'flat',
+			contact_person_number:'098123456732',
+			property_name:'adewale',
+			contact_person_address:'block 199 flat 4',
+			proof:'yes i do',
+			note:'just be a good buyer'
+		};
+		
 		chai.request(app)
 			.post('/api/v1/property-advert')
 			.send(body)
