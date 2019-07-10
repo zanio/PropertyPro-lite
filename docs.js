a877c4b356f92a1e0533cdd0a39793da97d58d63
Zanio <akp.ani@yahoo.com>
2019-07-10 12:20:01 +0100

    feature(google-map-red-flag):add route to get address and also implemented the google map api [Finish #166666178]

---
+++ docs.js
@@ -0,0 +1,10 @@
+const open = require('open');
+
+// Opens the image in the default image viewer
+(async () => {
+
+	// Opens the url in the default browser
+	await open('http://localhost:3300/api-docs');
+
+
+})();
